a8c4f74f8a3c02d29f977586f16ddd87e0a39f34
ByAncort <diego2005cortes@gmail.com>
2026-07-21 19:24:25 -0400

    initial: creacion setup inicial de spring java 21

---
+++ src/main/java/com/necro/devolucionesback/DevolucionesAppBackApplication.java
@@ -0,0 +1,13 @@
+package com.necro.devolucionesback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevolucionesAppBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevolucionesAppBackApplication.class, args);
+    }
+
+}
